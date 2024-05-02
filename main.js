5a0cca1a317371924e4117965f7a34e3cc333e70
TechNinja007 <125170720+TechNinja11@users.noreply.github.com>
2024-05-02 17:51:58 +0800

    Add files via upload
    Hi!!! My name is Joshua Gesta, I'm a front-developer and learning to html css and js

---
+++ main.js
@@ -0,0 +1,63 @@
+
+var swiper = new Swiper(".about-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".professional-slider", {
+  spaceBetween: 15,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+$(document).ready(function () {
+
+  $('#menu').click(function () {
+      $(this).toggleClass('bx-x');
+      $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load', function () {
+      $('#menu').removeClass('bx-x');
+      $('.navbar').removeClass('nav-toggle');
+  });
+});
